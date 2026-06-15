4d4b320b964431804071352c9ab3ca28d65d6a48
Matt Fricker <matt@fricker.io>
2026-06-15 17:13:32 +0100

    add error when customers cannot be loaded

--- src/app/data-functions/data-functions.component.ts
+++ src/app/data-functions/data-functions.component.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
+
 import { environment } from '../../environments/environment';
 
 @Component({
@@ -12,16 +15,17 @@ import { environment } from '../../environments/environment';
 export class DataFunctionsComponent {
   data: string[] = [];
   listItem: string | null = null;
-
-  customers: Customer[] = [];
-  isVisible: boolean = true;
+  customers: WritableSignal<Customer[]> = signal([]);
+  customerError: WritableSignal<string | null> = signal(null);
+  hasError: WritableSignal<boolean> = signal(false);
+  isVisible: WritableSignal<boolean> = signal(true);
 
   constructor(
     private HttpClient: HttpClient
   ) { }
 
   ngOnInit(): void {
-    this.isVisible = true;
+    this.isVisible.set(true);
   }
 
   addItem(): void {
@@ -40,10 +44,19 @@ export class DataFunctionsComponent {
   getCustomers(): void {
     const apiUrl = environment.apiUrl;
     this.HttpClient.get<Customer[]>(apiUrl)
-      .subscribe((result) => this.customers = result);
+      .pipe(
+        catchError((error) => {
+          this.customerError.set('Unable to load customers. Please try again later.');
+          this.hasError.set(true);
+          return of([]);
+        }
+      ))
+      .subscribe((result: Customer[]) => {
+        this.customers.set(result);
+      });
 
     // hide button
-    this.isVisible = false;
+    this.isVisible.set(false);
   }
 }
 
